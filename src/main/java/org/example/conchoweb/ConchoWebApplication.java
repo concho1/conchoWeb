a505d95b89f494fd9eb986c8d795e92a78aecb74
concho1 <dlwnsdn980828@gmail.com>
2024-02-21 14:23:06 +0900

    concho

---
+++ src/main/java/org/example/conchoweb/ConchoWebApplication.java
@@ -0,0 +1,13 @@
+package org.example.conchoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConchoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConchoWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/conchoweb/ConchoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.conchoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConchoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
